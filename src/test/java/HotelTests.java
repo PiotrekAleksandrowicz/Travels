3bb20f966abfb76cae7af6fd2305887ce19cc36e
PiotrekAleksandrowicz <piotrek.g.aleksandrowicz@gmail.com>
2021-07-21 15:02:51 +0200

    updating pom.xml file and adding class HotelTests

---
+++ src/test/java/HotelTests.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class HotelTests {
+
+    @Test
+    public void searchHotel(){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
+        driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/div/input")).sendKeys("Dubai");
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='select2-match' and text()='Dubai']")));
+        driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+
+    }
+}
