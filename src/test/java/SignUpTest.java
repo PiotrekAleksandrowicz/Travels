21224e52dc9f65e1eca8170fda6adff9566f9b9a
PiotrekAleksandrowicz <piotrek.g.aleksandrowicz@gmail.com>
2021-07-26 14:25:13 +0200

    complete SigUp form

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -3,6 +3,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -19,5 +22,21 @@ public class SignUpTest {
                 .filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
 
         driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+
+        driver.findElement(By.name("firstname")).sendKeys("Jan");
+        driver.findElement(By.name("lastname")).sendKeys("Kowalski");
+        driver.findElement(By.name("phone")).sendKeys("666666666");
+        driver.findElement(By.name("email")).sendKeys("kowalskiJan5@mail.pl");
+        driver.findElement(By.name("password")).sendKeys("123456");
+        driver.findElement(By.name("confirmpassword")).sendKeys("123456");
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h3[@class='RTL']")));
+
+        WebElement heading =  driver.findElement(By.xpath("//h3[@class='RTL']"));
+
+        Assert.assertTrue(heading.getText().contains("Hi, Jan Kowalski"));
+
     }
 }
