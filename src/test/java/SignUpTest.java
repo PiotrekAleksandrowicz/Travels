9bf5695e1770bc02c2f95b59c3f4f384700d6b4d
PiotrekAleksandrowicz <piotrek.g.aleksandrowicz@gmail.com>
2021-07-27 12:44:41 +0200

    Add soft assertions for result

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -7,6 +7,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class SignUpTest {
@@ -39,6 +43,52 @@ public class SignUpTest {
         WebElement heading =  driver.findElement(By.xpath("//h3[@class='RTL']"));
 
         Assert.assertTrue(heading.getText().contains("Hi, Jan Kowalski"));
+
+    }
+
+    @Test
+    public void signUpEmptyForm(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream()
+                .filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+
+
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#headersignupform > div.resultsignup")));
+
+
+
+
+        List<String> resultOfTest = driver.findElements(By.xpath("//div[@class='alert alert-danger']//p"))
+                .stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+
+        System.out.println(resultOfTest.size());
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertTrue(resultOfTest.contains("The Email field is required."));
+        softAssert.assertTrue(resultOfTest.contains("The Password field is required."));
+        softAssert.assertTrue(resultOfTest.contains("The Password field is required."));
+        softAssert.assertTrue(resultOfTest.contains("The First name field is required."));
+        softAssert.assertTrue(resultOfTest.contains("The Last Name field is required."));
+         softAssert.assertAll();
+
+
+
+
+
+
 
     }
 
