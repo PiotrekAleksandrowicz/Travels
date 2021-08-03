2efd72afd3f3e5bb8eb56e7d06a8486b0415cc51
Piotr <piotrek.g.aleksandrowicz@gmail.com>
2021-08-03 09:48:58 +0200

    Preparing SiggUpPage

--- src/test/java/pl/selenium/travels/pages/SignUpPage.java
+++ src/test/java/pl/selenium/travels/pages/SignUpPage.java
@@ -1,4 +1,32 @@
 package pl.selenium.travels.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class SignUpPage {
+
+
+    @FindBy(name = "firstname")
+    WebElement firstNameInput;
+
+    @FindBy(name = "lastName")
+    WebElement lastnameInput;
+
+    @FindBy(name = "phone")
+    WebElement phoneInput;
+
+    @FindBy(name = "email")
+    WebElement emailInput;
+
+    @FindBy(name = "password")
+    WebElement passwordInput;
+
+    @FindBy(name = "confirmpassword")
+    WebElement confirmPasswordInput;
+
+    @FindBy(xpath = "//button[text()=' Sign Up']")
+    WebElement signUpButton;
+
+
 }
+
